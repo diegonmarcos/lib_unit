f584eaf0251151eda185b93c1737aef178d9ab73
diegonmarcos <diegonmarcos@gmail.com>
2025-01-11 21:56:59 +0100

    up

--- clang_error_flags.sh
+++ clang_error_flags.sh
@@ -1,6 +1,7 @@
 alias gcl='git clone'
 alias push='git add . && git commit -m "update" && git push'
 alias norm="norminette -R CheckForbiddenSourceHeader CheckDefine"
+
 function c {
   clang "$1" -o "$(basename "$1" .c).out"
 }
@@ -14,7 +15,7 @@ function ce {
   # --- Clang/gcc flags
   echo -e "\n\n\n##########################"
   echo -e "##### Clang Flags ########"
-  echo -e "##########################\n\n"
+  echo -e "##########################"
 
   echo -e "\n##Clang Wall Wextra - fsyntaxOnly"
   echo -e "############################################\n"
@@ -28,24 +29,25 @@ function ce {
   echo -e "#########################\n"
   clang --analyze -Xanalyzer -analyzer-checker=core "$1" 2>&1
 
-  # --- Run Valgrind ---
-  echo -e "\n\n\n##########################"
-  echo -e "##### Valgrind ###########"
-  echo -e "##########################\n\n"
-  clang "$1" -o "$(basename "$1" .c)_dbV.db.out" -g
-  valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -v --log-file=valgrind_output.txt ./"$(basename "$1" .c)_dbV.db.out" "$@" > /dev/null || (grep $(basename "$1" .c) valgrind_output.txt ; rm valgrind_output.txt)
-
   # --- Run with ASan ---
   echo -e "\n\n\n##########################"
   echo -e "##### ASanitizer #########"
   echo -e "##########################\n\n"
   clang "$1" -o "$(basename "$1" .c)_dbA.db.out" -g -fsanitize=address -fsanitize-recover=address
-  ./"$(basename "$1" .c)_dbA.db.out" "${@:2}" 2> asan_output.txt || (grep $(basename "$1" .c) asan_output.txt ; rm asan_output.txt)
+  ./"$(basename "$1" .c)_dbA.db.out" "${@:2}" 2> asan_output.txt ; (grep "$1" asan_output.txt ; rm asan_output.txt)
+
+  # --- Run Valgrind ---
+  echo -e "\n\n\n##########################"
+  echo -e "##### Valgrind ###########"
+  echo -e "##########################\n\n"
+  clang "$1" -o "$(basename "$1" .c)_dbV.db.out" -g
+  valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -v --log-file=valgrind_output.txt ./"$(basename "$1" .c)_dbV.db.out" "${@:2}" > /dev/null | (grep "$1": valgrind_output.txt ; rm valgrind_output.txt)
 
   rm *.db.out
   rm *.plist
 
 }
+
 
 function cd {
   # --- LLDB
